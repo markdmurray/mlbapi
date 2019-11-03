90120f97141571e7803865ec8ce7e2e8ec606508
Mark Murray <m@rkmurray.co>
2019-11-03 18:38:09 +1100

    /mlb gives a valid json response. Only static at the moment(city=Chicago) nothing dynamic.

---
+++ teams/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from teams.models import TeamsBySeason
+# Create your views here.
+
+def TeamsBySeasonView(request):
+    data = TeamsBySeason.objects.filter(data__city='Chicago').values_list('data')
+    return JsonResponse(list(data), safe=False)
