8194fa38a4f7415c4f2096dc879e644b7d7c8d25
Mark Murray <m@rkmurray.co>
2019-12-06 04:32:35 +0000

    Update dependencies

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 # Ensure dependencies are installed;
 
-sudo yum install git gcc python3-devel python3-pip python-virtualenv
-sudo amazon-linux-extras install nginx1.12
+sudo yum install -y git gcc python3-devel python3-pip python-virtualenv
+sudo amazon-linux-extras install -y nginx1.12
