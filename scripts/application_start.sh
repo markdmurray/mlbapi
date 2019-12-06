2d34722df3f8051fe0566715af8e86fbb5987337
markdmurray <m@rkmurray.co>
2019-12-06 18:30:05 +1100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,8 +3,10 @@
 set -o errexit
 set -o pipefail
 
-source ../env/bin/activate
+WORK_DIRECTORY="/home/ec2-user/mlbapi"
 
+cd $WORK_DIRECTORY
+source env/bin/activate
 sudo systemctl enable nginx
 uwsgi --init /home/ec2-user/scripts/uwsgi.ini
 sudo systemctl restart nginx
