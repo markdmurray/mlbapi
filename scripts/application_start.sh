75cbf43396b6500b7b6dc05d0c62b59bc46341fe
markdmurray <m@rkmurray.co>
2019-12-07 17:46:59 +1100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,5 +10,6 @@ source env/bin/activate
 sudo systemctl enable nginx
 setfacl -m u:ec2-user:rwx /run
 uwsgi --ini /home/ec2-user/mlbapi/scripts/uwsgi.ini
+sleep 5
 setfacl -m u:nginx:rw /run/uwsgi.sock
 sudo systemctl restart nginx
