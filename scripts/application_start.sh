f5fa0c1e9104bc2dd1b9827c2cd0fc226c00d874
markdmurray <m@rkmurray.co>
2019-12-07 13:41:58 +1100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,5 +8,7 @@ WORK_DIRECTORY="/home/ec2-user/mlbapi"
 cd $WORK_DIRECTORY
 source env/bin/activate
 sudo systemctl enable nginx
+setfacl -m u:ec2-user:rw /run
 uwsgi --ini /home/ec2-user/mlbapi/scripts/uwsgi.ini
+setfacl -m u:nginx:rw /run/uwsgi.sock
 sudo systemctl restart nginx
